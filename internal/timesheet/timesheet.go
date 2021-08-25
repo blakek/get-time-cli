eea7a9b71e38f8717cb9c9dcd7929d0defd289c7
Blake Knight <bknight@gsandf.com>
2021-08-24 20:35:16 -0500

    :art: refactor: use Timesheet to handle summary of entries

--- internal/timesheet/timesheet.go
+++ internal/timesheet/timesheet.go
@@ -7,18 +7,27 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/blakek/get-time-cli/internal/options"
 )
 
-type TimeEntry struct {
+type TimesheetEntry struct {
 	Name       string
 	noteNumber string
 	Notes      string
 	Time       float32
 }
 
-func Create(startHour uint, endHour uint, options *options.Options) string {
+type Timesheet struct {
+	Entries        []*TimesheetEntry
+	CompletionTime time.Time
+	IsCompleted    bool
+	TimeRemaining  time.Duration
+	TimeWorked     time.Duration
+}
+
+func CreateFile(startHour uint, endHour uint, options *options.Options) string {
 	if startHour > 23 || endHour > 23 {
 		log.Fatal("start and end times must be a valid hour number")
 	}
@@ -55,7 +64,7 @@ func Create(startHour uint, endHour uint, options *options.Options) string {
 	return template.String()
 }
 
-func ParseFile(filePath string, options *options.Options) []*TimeEntry {
+func ParseFile(filePath string, options *options.Options) *Timesheet {
 	file, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
@@ -66,14 +75,14 @@ func ParseFile(filePath string, options *options.Options) []*TimeEntry {
 	noteNumberRegex := regexp.MustCompile(`\[\^(\d+)\](?::\s*(.*))?`)
 	skipEntryRegex := regexp.MustCompile(`^\s*(?: -+|Name)\s*$`)
 
-	timeEntries := []*TimeEntry{}
+	timeEntries := []*TimesheetEntry{}
 	hasNotes := false
 	hasTimeEntries := false
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		lineParts := strings.SplitN(line, "|", 3)
-		timeEntry := TimeEntry{}
+		timeEntry := TimesheetEntry{}
 
 		isTimeEntryLine := len(lineParts) == 3 && !skipEntryRegex.MatchString(lineParts[1])
 		noteNumberMatches := noteNumberRegex.FindStringSubmatch(line)
@@ -96,7 +105,7 @@ func ParseFile(filePath string, options *options.Options) []*TimeEntry {
 		if !isTimeEntryLine {
 			// Stop reading the file if all time entries and notes have been read
 			if hasNotes && hasTimeEntries {
-				return timeEntries
+				return getTimesheetFromEntries(timeEntries, options)
 			}
 
 			continue
@@ -116,15 +125,52 @@ func ParseFile(filePath string, options *options.Options) []*TimeEntry {
 		log.Fatal(err)
 	}
 
-	return timeEntries
+	return getTimesheetFromEntries(timeEntries, options)
 }
 
-func GetHoursWorked(times []*TimeEntry) float32 {
+func GetCompletionTime(timeRemaining time.Duration, options *options.Options) time.Time {
+	granularity := time.Hour / time.Duration(options.Granularity)
+	return time.Now().Add(timeRemaining).Round(granularity)
+}
+
+func GetTimeRemaining(timeWorked time.Duration, options *options.Options) time.Duration {
+	hoursPerDay := getDurationFromHours(options.HoursPerDay)
+	timeRemaining := hoursPerDay - timeWorked
+
+	if timeRemaining > 0 {
+		return timeRemaining
+	}
+
+	return 0
+}
+
+func GetTimeWorked(times []*TimesheetEntry) time.Duration {
 	var hoursWorked float32 = 0
 
 	for _, timeEntry := range times {
 		hoursWorked += timeEntry.Time
 	}
 
-	return hoursWorked
+	return getDurationFromHours(hoursWorked)
+}
+
+// This is a utility to convert a float of hours to a `time.Duration`.
+// `time.Duration` doesn't accept floats, so the duration needs to be converted
+// to minutes instead of fractions of hours
+func getDurationFromHours(hours float32) time.Duration {
+	return time.Duration(hours*60) * time.Minute
+}
+
+func getTimesheetFromEntries(entries []*TimesheetEntry, options *options.Options) *Timesheet {
+	timeWorked := GetTimeWorked(entries)
+	timeRemaining := GetTimeRemaining(timeWorked, options)
+	completionTime := GetCompletionTime(timeRemaining, options)
+
+	return &Timesheet{
+		CompletionTime: completionTime,
+		Entries:        entries,
+		IsCompleted:    timeRemaining > 0,
+		TimeWorked:     timeWorked,
+		TimeRemaining:  timeRemaining,
+	}
 }
--- main.go
+++ main.go
@@ -8,46 +8,22 @@ import (
 	"github.com/blakek/get-time-cli/internal/timesheet"
 )
 
-func getCompletionTime(hoursRemaining float32, options *options.Options) time.Time {
-	// `time.Duration` doesn't accept floats, so the duration is converted to
-	// minutes instead of fractions of hours
-	minutesRemaining := time.Minute * time.Duration(hoursRemaining*60)
-	granularity := 60 * time.Minute / time.Duration(options.Granularity)
-	return time.Now().Add(minutesRemaining).Round(granularity)
-}
+func printLongSummary(timesheet *timesheet.Timesheet, options *options.Options) {
+	fmt.Printf("Current:   %0.2f\n", timesheet.TimeWorked.Hours())
+	fmt.Printf("Remaining: %0.2f", timesheet.TimeRemaining.Hours())
 
-func getHoursRemaining(hoursWorked float32, options *options.Options) float32 {
-	hoursRemaining := options.HoursPerDay - hoursWorked
-
-	if hoursRemaining > 0 {
-		return hoursRemaining
-	}
-
-	return 0
-}
-
-func printLongSummary(hoursWorked float32, options *options.Options) {
-	hoursRemaining := getHoursRemaining(hoursWorked, options)
-	completionTime := getCompletionTime(hoursRemaining, options)
-
-	fmt.Printf("Current:   %0.2f\n", hoursWorked)
-	fmt.Printf("Remaining: %0.2f", hoursRemaining)
-
-	if hoursRemaining > 0 {
-		fmt.Printf(" (%v)", completionTime.Format(time.Kitchen))
+	if timesheet.IsCompleted {
+		fmt.Printf(" (%v)", timesheet.CompletionTime.Format(time.Kitchen))
 	}
 
 	fmt.Printf("\n")
 }
 
-func printShortSummary(hoursWorked float32, options *options.Options) {
-	hoursRemaining := getHoursRemaining(hoursWorked, options)
-	completionTime := getCompletionTime(hoursRemaining, options)
+func printShortSummary(timesheet *timesheet.Timesheet, options *options.Options) {
+	fmt.Printf("%0.2f hr", timesheet.TimeWorked.Hours())
 
-	fmt.Printf("%0.2f hr", hoursWorked)
-
-	if hoursRemaining > 0 {
-		fmt.Printf(" (@%v)", completionTime.Format(time.Kitchen))
+	if timesheet.IsCompleted {
+		fmt.Printf(" (@%v)", timesheet.CompletionTime.Format(time.Kitchen))
 	}
 
 	fmt.Printf("\n")
@@ -59,10 +35,9 @@ func main() {
 	// filePath := "/Users/bknight/Desktop/scratchpad/times-txt/2021/2021-08/2021-08-03.md"
 	filePath := "/Users/bknight/Desktop/scratchpad/times-txt/2021-08-25.md"
 
-	timeEntries := timesheet.ParseFile(filePath, opts)
-	hoursWorked := timesheet.GetHoursWorked(timeEntries)
+	timesheet := timesheet.ParseFile(filePath, opts)
 
-	printLongSummary(hoursWorked, opts)
+	printLongSummary(timesheet, opts)
 	fmt.Println("---")
-	printShortSummary(hoursWorked, opts)
+	printShortSummary(timesheet, opts)
 }
