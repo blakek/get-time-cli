46935581398f1b45a53728439f4b0b37fd1df835
Blake Knight <bknight@gsandf.com>
2021-10-05 08:07:43 -0500

    :sparkles: add initial support for getting timesheets from home config directory

--- internal/options/options.go
+++ internal/options/options.go
@@ -1,6 +1,10 @@
 package options
 
-import "path"
+import (
+	"os"
+	"path"
+	"time"
+)
 
 type Options struct {
 	// How many sections to break each hour into. Default is 4 for 15 minutes
@@ -10,18 +14,47 @@ type Options struct {
 	HoursPerDay float32
 	// Location where timesheets are stored
 	TimesheetDirectory string
+	// Time format string for naming timesheets
+	TimesheetNameFormat string
 }
 
 func TimesheetPath(timesheetName string, options *Options) string {
 	return path.Join(options.TimesheetDirectory, timesheetName)
 }
 
+func TimesheetPathForDate(date time.Time, options *Options) string {
+	return TimesheetPath(
+		date.Format(options.TimesheetNameFormat),
+		options,
+	)
+}
+
 func FetchOptions() *Options {
+	configDir := ensureConfigDir()
+
 	defaultOptions := &Options{
-		Granularity:        4,
-		HoursPerDay:        8,
-		TimesheetDirectory: "times-test",
+		Granularity:         4,
+		HoursPerDay:         8,
+		TimesheetDirectory:  configDir,
+		TimesheetNameFormat: "2006-01-02",
 	}
 
 	return defaultOptions
+}
+
+func ensureConfigDir() string {
+	configDir := getConfigDir()
+	_, error := os.Stat(configDir)
+
+	if os.IsNotExist(error) {
+		os.Mkdir(configDir, 0775)
+	}
+
+	return configDir
+}
+
+func getConfigDir() string {
+	userHomeDir, _ := os.UserHomeDir()
+	configDir := path.Join(userHomeDir, ".get-time")
+	return configDir
 }
